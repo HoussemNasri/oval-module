a90eefb1a35f49a07fb322c820d604b523813377
HoussemNasri <houssemnasri2001@gmail.com>
2023-06-07 19:04:16 +0100

    Add OvalTestManager and OvalStateManager

---
+++ src/main/java/com/suse/manager/OvalStateManager.java
@@ -0,0 +1,29 @@
+package com.suse.manager;
+
+import com.suse.model.StateType;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class OvalStateManager {
+    private final Map<String, StateType> statesMap = new HashMap<>();
+
+    public OvalStateManager(List<StateType> states) {
+        for (StateType state : states) {
+            statesMap.put(state.getId(), state);
+        }
+    }
+
+    public StateType get(String stateId) {
+        StateType state = statesMap.get(stateId);
+        if (state == null) {
+            throw new IllegalArgumentException("The state id is invalid: " + stateId);
+        }
+        return state;
+    }
+
+    public boolean exists(String stateId) {
+        return statesMap.containsKey(stateId);
+    }
+}
---
+++ src/main/java/com/suse/manager/OvalTestManager.java
@@ -0,0 +1,30 @@
+package com.suse.manager;
+
+
+import com.suse.model.TestType;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class OvalTestManager {
+    private final Map<String, TestType> testsMap = new HashMap<>();
+
+    public OvalTestManager(List<TestType> tests) {
+        for (TestType test : tests) {
+            testsMap.put(test.getId(), test);
+        }
+    }
+
+    public TestType get(String testId) {
+        TestType test = testsMap.get(testId);
+        if (test == null) {
+            throw new IllegalArgumentException("The test id is invalid: " + testId);
+        }
+        return test;
+    }
+
+    public boolean exists(String testId) {
+        return testsMap.containsKey(testId);
+    }
+}
